aa21d572f1cf8bfbd5995d75444cf0b7b1442262
thient <thient@vng.com.vn>
2020-03-10 16:03:55 +0700

    increase image size

---
+++ PytorchNeuralStyleTransferV2/sv_train_script/quy_oil_lu_1080_server.sh
@@ -0,0 +1,13 @@
+CUDA_VISIBLE_DEVICES=1 nohup python train.py \
+--name quy_oil_lu_C1_T200_ \
+--cuda \
+--content_image content/quy.jpg \
+--style_image style/oil_pastel.png \
+--content_weight 1 \
+--style_weight 200 \
+--imageSize 1080 \
+--outf output/  \
+--save_niter  50 \
+--lr 10 \
+--luminance_only \
+--niter 1001 > log/quy_oil_lu_1080_C1_T200.log  &
