6790bfaba8a872d752e5de77356e7f6177a63045
thient <thient@vng.com.vn>
2020-03-24 15:31:16 +0700

    cat_piscasso_lu2

---
+++ PytorchNeuralStyleTransferV2/train_script/cat_picasso_lu2.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0 python train.py \
+--name cat_picasso_lu_5_100_2   \
+--cuda \
+--content_image content/cat.jpg \
+--style_image style/picasso.jpg \
+--outf output/  \
+--content_weight 5 \
+--style_weight 100 \
+--imageSize 256 \
+--save_niter  10 \
+--luminance_only  \
+--niter 51
